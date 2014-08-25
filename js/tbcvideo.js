9879ffce32b6e7315a0f1d12d92f3986579ecce5
Felix <javagoshell@gmail.com>
2014-08-25 17:36:31 +0800

    基本功能实现

--- js/tbcvideo.js
+++ js/tbcvideo.js
@@ -72,7 +72,7 @@
             +'allowFullScreen="true"'
             +'type="application/x-shockwave-flash"'
             +'pluginspage="http://www.adobe.com/go/getflashplayer">'
-            +'</embed>';
+            +'</embed></object>';
 
         if(videoSupport() == false){//如果不支持html5video标签，则使用flash播放器
             return this.each(function(){
@@ -86,129 +86,224 @@
                 $tbcVideo.replaceWith(videoFlvHtml);
             }
         }
-        return this.each(function(){
-            var $tbcVideo = $(this);
-            //$tbcVideo.children('source').attr('src',player_params.videoPath);
-            //为媒体播放器应用样式主题
-            var $video_wrap = $('<div></div>').addClass('video-player').addClass(options.theme).addClass(options.childtheme);
-            var $video_seek = $('<div class="video-seek"></div>');
-            //书写控制条
-            var $video_controls = $('<div class="video-controls">' +
-                '<a class="video-play" title="播放/暂停"></a>' +
-                '<div class="video-timer">00:00</div>' +
-                '<div class="full-screen-button">全屏</div>' +
-                '<div class="volume-slider"></div>' +
-                '<a class="volume-button" title="静音/取消静音"></a>'+
-                '</div>');
-            $tbcVideo.wrap($video_wrap);
-            $tbcVideo.after($video_seek);
-            $video_seek.after($video_controls);
+        var $tbcVideo = this;
+        var initialVideo = function () {
+            $tbcVideo.addClass("video-initial-size");
+            $tbcVideo.prop("preload", "none");
+            $tbcVideo.prop("src", player_params.videoPath);
+            $tbcVideo.prop("poster", "images/logo.png");
+        }
+        //初始化video标签
+        initialVideo();
+        //$tbcVideo.children('source').attr('src',player_params.videoPath);
+        //为媒体播放器应用样式主题
+        var $video_wrap = $('<div></div>').addClass('video-player').addClass(options.theme).addClass(options.childtheme);
+        var $video_seek = $('<div class="video-seek"></div>');
+        var $posterPlayButton = $("<div class='poster-play-button'></div>")
+        //书写控制条
+        var $video_controls = $('<div class="video-controls">' +
+            '<a class="video-play" title="播放/暂停"></a>' +
+            '<span class="video-timer">00:00</span>' +
+            '<div class="full-screen-button">全屏</div>' +
+            '<div class="volume-slider"></div>' +
+            '<a class="volume-button" title="静音/取消静音"></a>' +
+            '</div>');
+        $tbcVideo.wrap($video_wrap);
+        $tbcVideo.before($posterPlayButton);
+        $video_controls.append($video_seek);
+        $tbcVideo.after($video_controls);
 
-            var $video_container = $tbcVideo.parent('.video-player');
-            var $video_controls = $('.video-controls', $video_container);
-            var $play_btn = $('.video-play', $video_container);
-            //var $video_seek = $('.video-seek', $video_container);
-            var $video_timer = $('.video-timer', $video_container);
-            var $volume = $('.volume-slider', $video_container);
-            var $volume_btn = $('.volume-button', $video_container);
+        var $video_container = $tbcVideo.parent('.video-player');
+        var $video_controls = $('.video-controls', $video_container);
+        var $play_btn = $('.video-play', $video_container);
+        var $video_seek = $('.video-seek', $video_container);
+        var $video_timer = $('.video-timer', $video_container);
+        var $volume = $('.volume-slider', $video_container);
+        var $volume_btn = $('.volume-button', $video_container);
+        var $video_full_screen = $('.full-screen-button', $video_container);
 
-            $video_controls.show(); //暂时不隐藏自定义控制条
+       // $video_controls.show(); //暂时不隐藏自定义控制条
 
-            var vPlay = function() {//播放/暂停按钮函数
-                if($tbcVideo.prop('paused') == false) {
-                    $tbcVideo[0].pause();
-                }
-                else {
-                    $tbcVideo[0].src = player_params.videoPath;
-                    $tbcVideo[0].play();
-                }
-            };
-            $play_btn.click(vPlay);
-            $tbcVideo.click(vPlay);
+        var vPlay = function () {//播放/暂停按钮函数
+            if ($tbcVideo.prop('paused') == true) {
+                $tbcVideo[0].play();
+            }
+            else {
+                $tbcVideo[0].pause();
+            }
+            $posterPlayButton.removeClass("poster-play-button")
+        };
+        $play_btn.click(vPlay);
+        $tbcVideo.click(vPlay);
+        $posterPlayButton.click(vPlay)
 
-            $tbcVideo.bind('play', function() {
-                $play_btn.addClass('paused-button');
-            });
+        $tbcVideo.bind('play', function () {
+            $play_btn.addClass('paused-button');
+        });
 
-            $tbcVideo.bind('pause', function() {
-                $play_btn.removeClass('paused-button');
-            });
+        $tbcVideo.bind('pause', function () {
+            $play_btn.removeClass('paused-button');
+        });
 
-            $tbcVideo.bind('ended', function() {
-                $play_btn.removeClass('paused-button');
-            });
-            var seeksliding;
-            var vSliderSeek = function() {//进度条
-                if($tbcVideo.prop('readyState')) {
-                    var video_duration = $tbcVideo.prop('duration');
-                    $video_seek.slider({
-                        value: 0,
-                        step: 0.01,
-                        orientation: "horizontal",
-                        range: "min",
-                        max: video_duration,
-                        animate: true,
-                        slide: function(){
-                            seeksliding = true;
-                        },
-                        stop:function(e,ui){
-                            seeksliding = false;
-                            $tbcVideo.prop("currentTime",ui.value);
-                        }
-                    });
-                    //$video_controls.show();
-                }else {
-                    setTimeout(vSliderSeek, 150);
-                }
-            };
-            vSliderSeek();
-            //时间显示
-            var vTimeFormat=function(seconds){
-                var m=Math.floor(seconds/60)<10?"0"+Math.floor(seconds/60):Math.floor(seconds/60);
-                var s=Math.floor(seconds-(m*60))<10?"0"+Math.floor(seconds-(m*60)):Math.floor(seconds-(m*60));
-                return m+":"+s;
-            };
+        $tbcVideo.bind('ended', function () {
+            $play_btn.removeClass('paused-button');
+        });
+        var seeksliding;
+        var vSliderSeek = function () {//进度条
+            if ($tbcVideo.prop('readyState')) {
+                var video_duration = $tbcVideo.prop('duration');
+                $video_seek.slider({
+                    value: 0,
+                    step: 0.01,
+                    orientation: "horizontal",
+                    range: "min",
+                    max: video_duration,
+                    animate: true,
+                    slide: function () {
+                        seeksliding = true;
+                    },
+                    stop: function (e, ui) {
+                        seeksliding = false;
+                        $tbcVideo.prop("currentTime", ui.value);
+                    }
+                });
+                //$video_controls.show();
+            } else {
+                setTimeout(vSliderSeek, 150);
+            }
+        };
+        vSliderSeek();
+        //时间显示
+        var vTimeFormat = function (seconds) {
+            var m = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
+            var s = Math.floor(seconds - (m * 60)) < 10 ? "0" + Math.floor(seconds - (m * 60)) : Math.floor(seconds - (m * 60));
+            return m + ":" + s;
+        };
 
-            var seekUpdate = function() {
-                var currenttime = $tbcVideo.prop('currentTime');
-                if(!seeksliding) $video_seek.slider({ value: currenttime });
+        var seekUpdate = function () {
+            var currenttime = $tbcVideo.prop('currentTime');
+            var durationTime = $tbcVideo.prop("duration")
+            if (!seeksliding) $video_seek.slider({ value: currenttime });
+            if (durationTime) {
+                $video_timer.text(vTimeFormat(currenttime) + "/" + vTimeFormat(durationTime));
+            } else {
                 $video_timer.text(vTimeFormat(currenttime));
-            };
+            }
+        };
 
-            $tbcVideo.bind('timeupdate', seekUpdate);
+        $tbcVideo.bind('timeupdate', seekUpdate);
 
-            //音量控制
-            $volume.slider({
-                value: 1,
-                orientation: "horizontal",
-                range: "min",
-                max: 1,
-                step: 0.05,
-                animate: true,
-                slide:function(e,ui){
-                    $tbcVideo.prop('muted',false);
-                    video_volume = ui.value;
-                    $tbcVideo.prop('volume',ui.value);
+        //音量控制
+        $volume.slider({
+            value: 1,
+            orientation: "horizontal",
+            range: "min",
+            max: 1,
+            step: 0.05,
+            animate: true,
+            slide: function (e, ui) {
+                $tbcVideo.prop('muted', false);
+                video_volume = ui.value;
+                $tbcVideo.prop('volume', ui.value);
+                if (video_volume < .5) {
+                    $volume_btn.addClass("volume-button-small");
+                } else {
+                    $volume_btn.removeClass("volume-button-small");
                 }
-            });
+            }
+        });
+        var video_volume = 1
+        var muteVolume = function () {
+            if ($tbcVideo.prop('muted') == true) {
+                $tbcVideo.prop('muted', false);
+                $volume.slider('value', video_volume);
+                $volume_btn.removeClass('volume-mute');
+            }
+            else {
+                $tbcVideo.prop('muted', true);
+                $volume.slider('value', '0');
+                $volume_btn.addClass('volume-mute');
+            }
+            ;
+        };
 
-            var muteVolume = function() {
-                if($tbcVideo.prop('muted')==true) {
-                    $tbcVideo.prop('muted', false);
-                    $volume.slider('value', video_volume);
-                    $volume_btn.removeClass('volume-mute');
+        $volume_btn.click(muteVolume);
+
+        $tbcVideo.remove('controls');
+        //进入全屏
+        var launchFullScreen = function (element) {
+            if (element.requestFullscreen) {
+                element.requestFullscreen();
+            } else if (element.msRequestFullscreen) {
+                element.msRequestFullscreen();
+            } else if (element.mozRequestFullScreen) {
+                element.mozRequestFullScreen();
+            } else if (element.webkitRequestFullscreen) {
+                element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        }
+        //退去全屏
+        var cancelFullScreen = function () {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
+        //是否全屏
+        var videoFullScreen = false;
+        $video_full_screen.click(function () {
+            if (videoFullScreen) {
+                videoFullScreen = false;
+                cancelFullScreen();
+                $video_container.removeClass("video-player-full-screen");
+            } else {
+                videoFullScreen = true;
+                launchFullScreen($video_container[0])
+                $video_container.addClass("video-player-full-screen");
+            }
+        });
+
+        //绑定屏幕变化事件
+        $(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (event) {
+            if (!(document.fullscreenElement ||
+                document.mozFullScreenElement ||
+                document.webkitFullscreenElement ||
+                document.msFullscreenElement)) {
+                videoFullScreen = false;
+                $video_container.removeClass("video-player-full-screen");
+            }
+        })
+
+        //2秒鼠标不同隐藏控制条
+        var hidemouse;
+        $(document).mousemove(function () {
+            clearTimeout(hidemouse);
+            $(".video-controls").fadeIn("slow");
+            hidemouse = setTimeout("$('.video-controls').fadeOut('slow')", 2000);
+        });
+
+        //向外暴露的工具函数
+        $.fn.extend({
+            tbcVideoPlay:function(){
+                if (this.prop('paused') == true) {
+                    this[0].play();
                 }
                 else {
-                    $tbcVideo.prop('muted', true);
-                    $volume.slider('value', '0');
-                    $volume_btn.addClass('volume-mute');
-                };
-            };
-
-            $volume_btn.click(muteVolume);
-
-            $tbcVideo.remove('controls');
+                    this[0].pause();
+                }
+                this.prev().removeClass("poster-play-button")
+            },
+            getCurrentVideoTime:function(){
+                return Math.floor(this.prop("currentTime"));
+            }
 
         });
+
+        return this;
     };
 })(jQuery);
